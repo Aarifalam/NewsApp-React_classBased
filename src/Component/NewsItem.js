e4cd4dc0c4c954abd5c54aa7495af96396fcb7a8
aarifalam <aarif20@navgurukul.org>
2022-04-17 22:50:05 +0530

    In this News i store news data in article variable and also tried to give NewsDetails button ID by using of this article datas href=/newsdetails/url

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 
 class NewsItems extends Component {
     render() {
-        let { title, description } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
+        // this newsUrl is used because to give NewsDetails button id
         return (
             <div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src="https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png" className="card-img-top" alt="..." />
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
                         {/* this button has "/" thats why after clicking this is refreshing the web app */}
                     </div>
                 </div>
