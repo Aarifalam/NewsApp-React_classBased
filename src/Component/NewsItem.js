52498399f9129e00c7a6fdf179952591888ba15f
aarifalam <aarif20@navgurukul.org>
2022-04-18 15:05:00 +0530

    this i am fething the all data from the NewsApi inside the componentDidMount() and aslo remove saved data of news.

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 class NewsItems extends Component {
+
     render() {
         let { title, description, imageUrl, newsUrl } = this.props;
         // this newsUrl is used because to give NewsDetails button id
         return (
             <div>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card mx-3 my-3" style={{ width: "18rem" }}>
+                    <img src={!imageUrl ? "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href={newsUrl} className="btn btn-sm btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} className="btn btn-sm btn-primary">Details of NEWS</a>
                         {/* this button has "/" thats why after clicking this is refreshing the web app */}
                     </div>
                 </div>
